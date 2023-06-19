c71dd50735cb898e9504e99d8ad416c30b70c0b1
Justin <dong135790@gmail.com>
2023-06-19 18:55:29 -0400

    added promps as layouts for task

--- generator/index.js
+++ generator/index.js
@@ -1,5 +1,51 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+// So we can write the file
+const fs = require('fs')
 
+const prompt = inquirer.createPromptModule();
+
+prompt([
+    {
+        name: 'title',
+        message: 'Please enter an appropriate readMe title',
+    },
+    {
+        name: 'description',
+        message: 'Enter appropriate readMe description',
+    },
+    {
+        name: 'installation',
+        message: 'Enter appropriate readMe installation instructions',
+    },
+    {
+        name: 'usage',
+        message: 'Enter appropriate readMe usage information',
+    },
+    {
+        name: 'contributing',
+        message: 'Enter appropriate readMe contribution guidelines',
+    },
+    {
+        name: 'test',
+        message: 'Enter appropriate readMe test instructions',
+    },
+    {
+        name: 'license',
+        message: 'Choose a license for your application',
+    },
+    {
+        name: 'gitUsername',
+        message: 'Enter your Github Username'
+    },
+    {
+        name: 'gitEmail',
+        message: 'Enter your Github email address'
+    }
+])
+.then((data) => {
+    console.log(data);
+})
 // TODO: Create an array of questions for user input
 const questions = [];
 
